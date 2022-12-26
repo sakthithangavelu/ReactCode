e07c99fd81d021424b2a8ba2e27b569ed4e8cbe0
sakthithangavelu <sakthi.thangavelu@aspiresys.com>
2022-12-26 10:30:50 +0530

    update

---
+++ codepractice/src/components/Strictmode.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function ExampleApplication() {
+  return (
+    <div>
+      <Header />
+      <React.StrictMode>
+        <div>
+          <ComponentOne />
+          <ComponentTwo />
+        </div>
+      </React.StrictMode>
+      <Footer />
+    </div>
+  );
+}
+
+//here strict mode performs check for the component 1 and 2 and its descendant
